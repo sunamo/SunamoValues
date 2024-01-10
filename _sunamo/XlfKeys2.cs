df75da74ebd7c11f5284c0102d672da269d2ffbc
sunamo <radek.jancik@sunamo.cz>
2024-01-10 12:35:18 +0100

    .

---
+++ _sunamo/XlfKeys2.cs
@@ -0,0 +1,9 @@
+namespace SunamoValues._sunamo;
+internal class XlfKeys2
+{
+    internal static string Dashv;
+    internal static string Vdashl;
+    internal static string Color;
+    internal static string Vbar;
+    internal static string Poincareplane;
+}
