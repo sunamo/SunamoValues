deb3ac799c96df4e94aa2f28327cfaa9f732ac7c
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 13:15:20 +0200

    All buildable, every in separate file

--- Constants/CsKeywords2.cs
+++
@@ -1,9 +0,0 @@
-
-namespace SunamoValues;
-//namespace SunamoValues.Constants;
-
-//public class CsKeywords2
-//{
-//    public static string c = "class";
-//    public static string ns = "namespace";
-//}
