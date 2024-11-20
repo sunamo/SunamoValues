2086747c8b757eb3b571500dd68d2a14bd23ecb2
sunamo.cz <radek.jancik@sunamo.cz>
2024-11-20 14:46:23 +0100

    Remove unused class

--- EmptyArrays.cs
+++
@@ -1,9 +0,0 @@
-namespace SunamoValues;
-
-public class EmptyArrays
-{
-    public static char[] Chars = { };
-    public static string[] Strings = { };
-    public static object[] Objects = { };
-    public static byte[] Bytes = { };
-}
