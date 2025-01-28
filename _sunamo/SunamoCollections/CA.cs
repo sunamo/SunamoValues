12d2c339d584e2aab782342226320075aedb8bf1
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:41 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/SunamoCollections/CA.cs
+++ _sunamo/SunamoCollections/CA.cs
@@ -2,17 +2,6 @@ namespace SunamoValues._sunamo.SunamoCollections;
 
 internal class CA
 {
-    internal static bool IsListStringWrappedInArray<T>(List<T> v2)
-    {
-        var first = v2.First().ToString();
-        if (v2.Count == 1 && (first == "System.Collections.Generic.List`1[System.String]" ||
-                              first == "System.Collections.Generic.List`1[System.Object]")) return true;
-        return false;
-    }
-    internal static void InitFillWith(List<string> datas, int pocet, string initWith = "")
-    {
-        InitFillWith<string>(datas, pocet, initWith);
-    }
     internal static void InitFillWith<T>(List<T> datas, int pocet, T initWith)
     {
         for (int i = 0; i < pocet; i++)
@@ -20,21 +9,6 @@ internal class CA
             datas.Add(initWith);
         }
     }
-    internal static void InitFillWith<T>(List<T> arr, int columns)
-    {
-        for (int i = 0; i < columns; i++)
-        {
-            arr.Add(default);
-        }
-    }
 
-    internal static List<string> ToListString(params string[] s)
-    {
-        return s.ToList();
-    }
 
-    internal static List<object> ToListObject(params Object[] lb)
-    {
-        return lb.ToList();
-    }
 }
--- _sunamo/SunamoCollectionsGeneric/CAGConsts.cs
+++ _sunamo/SunamoCollectionsGeneric/CAGConsts.cs
@@ -2,10 +2,6 @@ namespace SunamoValues._sunamo.SunamoCollectionsGeneric;
 
 internal class CAGConsts
 {
-    internal static T[] ToArrayT<T>(params T[] aB)
-    {
-        return aB;
-    }
 
     /// <summary>
     /// Tady to musí být, SunamoValues nemůže dědit od SunamoCollectionGeneric - vzniklo by Cycle detected
