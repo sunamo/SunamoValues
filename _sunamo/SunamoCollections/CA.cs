1756149b329734cf50c40dc40e28adc945dc391d
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 21:01:08 +0100

    Buildable, removed unused internal,private,protected methods in _sunamo

--- _sunamo/SunamoCollections/CA.cs
+++ _sunamo/SunamoCollections/CA.cs
@@ -2,13 +2,6 @@ namespace SunamoValues._sunamo.SunamoCollections;
 
 internal class CA
 {
-    internal static void InitFillWith<T>(List<T> datas, int pocet, T initWith)
-    {
-        for (int i = 0; i < pocet; i++)
-        {
-            datas.Add(initWith);
-        }
-    }
 
 
 }
