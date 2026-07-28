efefe9b27a64d595038328741aabce9f172c0f1b
smutekutek <smutekutek@gmail.com>
2026-07-28 17:37:15 +0200

    feat: doplnit API pro SunamoCzAdmin konsolidaci (lokální feed 26.7.28.x)

---
+++ SunamoValues/Values/Consts.cs
@@ -0,0 +1,9 @@
+namespace SunamoValues;
+
+public class Consts
+{
+    public const string https = "https://";
+    public const string http = "http://";
+    public const string Cz = "Cz";
+    public const string Test_ = "Test_";
+}
